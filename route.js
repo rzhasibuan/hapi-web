6747374d4d417fcab7b3e4eb208a21aac985b5f4
rzhasibuan <rzhasibuan@gmail.com>
2021-05-10 14:05:53 +0700

    route with params

--- route.js
+++ route.js
@@ -8,11 +8,40 @@ const routes = [
     },
   },
   {
+    method: '*',
+    path: '/',
+    handler: (request, h) => {
+      return 'Halaman tidak dapat di akses dengan method tersebut';
+    }
+  },
+  {
     method: "GET",
     path: "/about",
     handler: (request, h) => {
       return "About Page";
     },
+  },
+  {
+    method: '*',
+    path: '/about',
+    handler : (request,h) => {
+      return "halaman tidak dapat di akses dengan method ";
+    }
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: (request,h) => {
+      return "halaman tidak dapat ditemukan";
+    }
+  },
+  {
+    method: 'GET',
+    path: '/hello/{name?}',
+    handler: (request,h) => {
+      const {name = "stranger"} = request.params;
+      return `hello, ${name}!`;
+    }
   },
 ];
 
